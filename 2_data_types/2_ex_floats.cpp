1b1cce5ed6c9d926b7d6de2a23d09024bd282712
Jonny Daenen <jonnydaenen@gmail.com>
2018-10-07 15:04:20 +0200

    add float summary

---
+++ 2_data_types/2_ex_floats.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+        double a{1.5}, b{}, c{};
+        double result {a / b};
+        cout << result << endl; // inf
+
+        cout << result + a << endl; // inf
+
+        result = b / c;
+        cout << result << endl; // nan
+}
